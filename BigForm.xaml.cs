2bb06ddb661bee037234906ceff7679e3c3f5e20
Pakmanchik <kaplunov555@mail.ru>
2023-04-24 12:18:54 +0300

    Добавьте файлы проекта.

---
+++ BigForm.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Printing;
+
+namespace Konvert
+{
+    /// <summary>
+    /// Логика взаимодействия для BigForm.xaml
+    /// </summary>
+    public partial class BigForm : Window
+    {
+        private readonly Inventory KonvertBisness = new(); // Подключение класса Inventory
+        public BigForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnPrint_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new();
+            printDialog.PrintQueue = new PrintQueue(new PrintServer(), KonvertBisness.defaultPrinterName);
+            printDialog.PrintTicket.PageOrientation = PageOrientation.Landscape;
+            printDialog.PrintTicket.PageMediaSize = new PageMediaSize(612, 869);
+            printDialog.PrintVisual(PrintBox, "Print");
+            DialogResult = true;
+            
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        { DialogResult = false; }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ StartForm.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Konvert
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StartForm : Window
+    {
+        private readonly Inventory KonvertBisness = new(); // Подключение класса Inventory
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void HyperExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+ 
+        private void HiperAdd_Click(object sender, RoutedEventArgs e)
+        {
+            AddForm addForm = new();
+            addForm.Show();
+            Close();
+        }
+
+        private void HyperPrint_Click(object sender, RoutedEventArgs e)
+        {
+            PrintForm printForm = new();
+            printForm.Show();
+            Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            KonvertBisness.DBAddArray();
+            MessageBox1 messageBox1 = new("Error", "information");
+            messageBox1.ShowDialog();
+            if(messageBox1.DialogResult == true)
+            {
+                MessageBox.Show("yes");
+            }
+            if (messageBox1.DialogResult == false)
+            { MessageBox.Show("no"); }
+        }
+    }
+}
