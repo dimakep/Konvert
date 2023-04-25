79370f0408be76516ae8f56c927154e485aa59bc
Pakmanchik <kaplunov555@mail.ru>
2023-04-25 22:45:40 +0300

    Изменил

--- BigForm.xaml.cs
+++ BigForm.xaml.cs
@@ -14,14 +14,23 @@ namespace Konvert
             string home, string frame, string structure, string flat, string printer)
         {
             InitializeComponent();
+            RecipientBox.Text = firm;
+            IndexBox.Text = Convert.ToString(index);
+            RegionBox.Text = region + " " + area;
+            CityBox.Text = city + " " + street;
+            HomeBox.Text += home;
+            FrameBox.Text += frame;
+            StructureBox.Text += structure;
+            FlatBox.Text += flat;
+            printerName = printer;
         }
 
         private void BtnPrint_Click(object sender, RoutedEventArgs e)
         {
             PrintDialog printDialog = new();
-            //printDialog.PrintQueue = new PrintQueue(new PrintServer(), KonvertBisness.defaultPrinterName);
-            //printDialog.PrintTicket.PageOrientation = PageOrientation.Landscape;
-            //printDialog.PrintTicket.PageMediaSize = new PageMediaSize(612, 869);
+            printDialog.PrintQueue = new PrintQueue(new PrintServer(), KonvertBisness.defaultPrinterName);
+            printDialog.PrintTicket.PageOrientation = PageOrientation.Landscape;
+            printDialog.PrintTicket.PageMediaSize = new PageMediaSize(612, 869);
             printDialog.PrintVisual(PrintBox, "Print");
             DialogResult = true;
             
