dd4998c178f82c0a904ca9679416dc4f36e1dc63
Pakmanchik <kaplunov555@mail.ru>
2023-05-03 19:43:53 +0300

    Revert "1"
    This reverts commit 09bd99972b0e1382ce99b57b37aa08e4bdaa47f3.

--- BigForm.xaml.cs
+++ BigForm.xaml.cs
@@ -2,7 +2,6 @@
 using System.Windows.Controls;
 using System.Printing;
 using System;
-using System.Windows.Media;
 
 namespace Konvert
 {
@@ -44,48 +43,6 @@ namespace Konvert
             DialogResult = true;
         }
 
-        private void Print1()
-        {
-            PrintDialog printDialog = new PrintDialog();
-            {
-                // Создать визуальный элемент для страницы
-                DrawingVisual visual = new DrawingVisual();
-
-                // Получить контекст рисования
-                using (DrawingContext dc = visual.RenderOpen())
-                {
-                    
-                    // Определить текст, который необходимо печатать
-                    FormattedText text = new FormattedText(txtContent.Text,
-                        System.Globalization.CultureInfo.CurrentCulture,
-                        FlowDirection.LeftToRight,
-                        new Typeface("Calibri"), 20, Brushes.Black);
-
-                    // Указать максимальную ширину, в пределах которой выполнять перенос текста, 
-                    text.MaxTextWidth = printDialog.PrintableAreaWidth / 2;
-
-                    // Получить размер выводимого текста. 
-                    Size textSize = new Size(text.Width, text.Height);
-
-                    // Найти верхний левый угол, куда должен быть помещен текст. 
-                    double margin = 96 * 0.25;
-                    Point point = new Point(
-                        (printDialog.PrintableAreaWidth - textSize.Width) / 2 - margin,
-                        (printDialog.PrintableAreaHeight - textSize.Height) / 2 - margin);
-
-                    // Нарисовать содержимое, 
-                    dc.DrawText(text, point);
-
-                    // Добавить рамку (прямоугольник без фона). 
-                    dc.DrawRectangle(null, new Pen(Brushes.Black, 1),
-                        new Rect(margin, margin, printDialog.PrintableAreaWidth - margin * 2,
-                            printDialog.PrintableAreaHeight - margin * 2));
-                }
-
-                // Напечатать визуальный элемент. 
-                printDialog.PrintVisual(visual, "Печать с помощью классов визуального уровня");
-            }
-        }
         private void BtnBack_Click(object sender, RoutedEventArgs e)
         { Close(); }
 
