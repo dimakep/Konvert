b9529ef6086032e13152c137bb93f3e3ab5f0303
Pakmanchik <kaplunov555@mail.ru>
2023-05-09 11:00:56 +0300

    1

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace StartForm
 {
@@ -13,5 +10,41 @@ namespace StartForm
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Выделение текста в TextBox при получении фокуса
+            EventManager.RegisterClassHandler(typeof(TextBox), TextBox.PreviewMouseLeftButtonDownEvent,
+                new MouseButtonEventHandler(SelectivelyIgnoreMouseButton));
+            EventManager.RegisterClassHandler(typeof(TextBox), TextBox.GotKeyboardFocusEvent,
+                new RoutedEventHandler(SelectAllText));
+            EventManager.RegisterClassHandler(typeof(TextBox), TextBox.MouseDoubleClickEvent,
+                new RoutedEventHandler(SelectAllText));
+            base.OnStartup(e);
+        }
+        void SelectivelyIgnoreMouseButton(object sender, MouseButtonEventArgs e)
+        {
+            // Поиск TextBox
+            DependencyObject parent = e.OriginalSource as UIElement;
+            while (parent != null && !(parent is TextBox))
+                parent = VisualTreeHelper.GetParent(parent);
+
+            if (parent != null)
+            {
+                var textBox = (TextBox)parent;
+                if (!textBox.IsKeyboardFocusWithin)
+                {
+                    // Если текстовое поле еще не сфокусировано, переведите его в фокус и
+                    // остановить дальнейшую обработку этого события клика.
+                    textBox.Focus();
+                    e.Handled = true;
+                }
+            }
+        }
+        void SelectAllText(object sender, RoutedEventArgs e)
+        {
+            var textBox = e.OriginalSource as TextBox;
+            if (textBox != null)
+                textBox.SelectAll();
+        }
     }
 }
