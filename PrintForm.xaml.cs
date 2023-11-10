bc9299c6d815daca76b2443cd5f9a252aa92cc44
dimakep <dimakep0105@gmail.com>
2023-11-10 14:33:02 +0300

    Удалены Small, big form, и закоментированные строки

--- PrintForm.xaml.cs
+++ PrintForm.xaml.cs
@@ -13,7 +13,6 @@ namespace Konvert
     /// </summary>
     public partial class PrintForm : Window
     {
-        //private readonly Inventory Inventory = new(); // Подключение класса Inventory
         int envelopeFormat = 1;
 
         public PrintForm()
@@ -25,11 +24,11 @@ namespace Konvert
             SqlDataAdapter adapter = new(command);
             DataSet dataSet = new();
             _ = adapter.Fill(dataSet, "Recipient");
-            // Создаем объект CollectionViewSource для привязки ComboBox к данным
+            /// Создаем объект CollectionViewSource для привязки ComboBox к данным
             CollectionViewSource viewSource = new();
             viewSource.Source = dataSet.Tables["Recipient"].DefaultView;
 
-            // Привязываем ComboBox к объекту CollectionViewSource
+            /// Привязываем ComboBox к объекту CollectionViewSource
             FirmBox.ItemsSource = viewSource.View;
             FirmBox.DisplayMemberPath = "Firm";
             FirmBox.SelectedValuePath = "Id";
@@ -49,37 +48,9 @@ namespace Konvert
         }
         private void BtnPrint_Click(object sender, RoutedEventArgs e)
         {
-            
             Variables.Firm = FirmBox.Text;
             Preview_Form preview_Form = new(envelopeFormat);
             preview_Form.ShowDialog();
-            //Small1Form small1Form = new();
-            //small1Form.Show();
-
-            /*
-            if ((bool)BigTab.IsChecked)
-            {
-                BigForm BigForm = new(Inventory.firmInv, Inventory.indexInv, Inventory.regionInv,
-                    Inventory.areaInv, Inventory.cityInv, Inventory.streetInv, Inventory.homeInv,
-                    Inventory.frameInv, Inventory.structureInv, Inventory.flatInv, PrinterNameBox.Text);
-                _ = BigForm.ShowDialog();
-
-            }
-            if ((bool)Small1Tab.IsChecked)
-            {
-                Small1Form small1Form = new(Inventory.firmInv, Inventory.indexInv, Inventory.regionInv,
-                    Inventory.areaInv, Inventory.cityInv, Inventory.streetInv, Inventory.homeInv,
-                    Inventory.frameInv, Inventory.structureInv, Inventory.flatInv, PrinterNameBox.Text);
-                _ = small1Form.ShowDialog();
-            }
-            if ((bool)Small2Tab.IsChecked)
-            {
-                Small2Form small2Form = new(Inventory.firmInv, Inventory.indexInv, Inventory.regionInv,
-                    Inventory.areaInv, Inventory.cityInv, Inventory.streetInv, Inventory.homeInv,
-                    Inventory.frameInv, Inventory.structureInv, Inventory.flatInv, PrinterNameBox.Text);
-                _ = small2Form.ShowDialog();
-            }
-            */
         }
         private void Small1Tab_Click(object sender, RoutedEventArgs e)
         {
